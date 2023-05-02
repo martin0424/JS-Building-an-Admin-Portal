27992a9415458efd3a9281e672dcfff32b9608ca
martin0424 <h.newland424@gmail.com>
2023-05-02 14:33:38 -0400

    Updated the public folder

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,27 @@
+const main = async () => {
+    const response = await fetch("http://localhost:3001/listBooks");
+    const books = await response.json();
+    books.forEach(renderBook)
+  }; 
+  
+  const renderBook = (book) => {
+      const root = document.querySelector("#root")
+  
+      const li = document.createElement("li")
+      li.textContent =book.title
+  
+      const changeQuantiy = document.createElement("input")
+      changeQuantiy.value = book.quantity
+      
+      const submitBtn = document.createElement("button")
+      submitBtn.textConten = "Submit Chnges"
+  
+      li.append(changeQuantiy, submitBtn)
+  
+      root.append(li)
+  
+      
+  }
+  
+  main()
 
-// Your Code Here
